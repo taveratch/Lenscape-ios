eb094cd9f575c3ff74d6f23bfefcc986897cac75
Taweerat Chaiman <taweesoft@gmail.com>
2018-03-04 20:34:31 +0700

    add gitignore & initial project

---
+++ Podfile
@@ -0,0 +1,6 @@
+# platform :ios, '9.0'
+
+target 'Lenscape' do
+  use_frameworks!
+  pod 'Alamofire'
+end
---
+++ Pods/Target Support Files/Pods-Lenscape/Pods-Lenscape-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lenscape : NSObject
+@end
+@implementation PodsDummy_Pods_Lenscape
+@end

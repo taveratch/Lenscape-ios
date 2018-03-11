b9c5db0557983b808d17a154ce02e194ef2c2531
Taweerat Chaiman <taweesoft@gmail.com>
2018-03-11 19:38:13 +0700

    add gradient

--- Lenscape/ViewControllers/ExploreViewController.swift
+++ Lenscape/ViewControllers/ExploreViewController.swift
@@ -12,25 +12,44 @@ import SwiftCarousel
 
 class ExploreViewController: AuthViewController {
     
-    @IBOutlet weak var selectedItemLabel: UILabel!
+    
+    @IBOutlet weak var someView: UIView!
+    //    @IBOutlet weak var selectedItemLabel: UILabel!
     @IBOutlet weak var carousel: SwiftCarousel!
     @IBOutlet weak var mapViewImage: UIImageView!
     @IBOutlet weak var profileImage: UIImageView!
-    
+    @IBOutlet weak var textLabel: UILabel!
     
     var items: [String]?
-    var itemsViews: [UILabel]?
+    var itemsViews: [UIView]?
+    let colors = [#colorLiteral(red: 0.4274509804, green: 0.8039215686, blue: 1, alpha: 1),#colorLiteral(red: 0.6823529412, green: 0.6823529412, blue: 0.6588235294, alpha: 1),#colorLiteral(red: 0.7882352941, green: 0.631372549, blue: 0.4352941176, alpha: 1),#colorLiteral(red: 0.8980392157, green: 0.5803921569, blue: 0.2156862745, alpha: 1),#colorLiteral(red: 1, green: 0.5333333333, blue: 0, alpha: 1),#colorLiteral(red: 1, green: 0.6196078431, blue: 0.1882352941, alpha: 1),#colorLiteral(red: 1, green: 0.7215686275, blue: 0.4117647059, alpha: 1),#colorLiteral(red: 1, green: 0.8431372549, blue: 0.6823529412, alpha: 1),#colorLiteral(red: 0.8823529412, green: 0.8352941176, blue: 0.7450980392, alpha: 1),#colorLiteral(red: 0.7725490196, green: 0.8274509804, blue: 0.8078431373, alpha: 1),#colorLiteral(red: 0.6588235294, green: 0.8196078431, blue: 0.8705882353, alpha: 1),#colorLiteral(red: 0.5490196078, green: 0.8117647059, blue: 0.9333333333, alpha: 1),#colorLiteral(red: 0.4274509804, green: 0.8039215686, blue: 1, alpha: 1)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         items = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        itemsViews = items!.map { labelForString($0) }
+        itemsViews = items!.enumerated().map { labelForString(text: $1, index: $0) }
         carousel.items = itemsViews!
-        carousel.resizeType = .visibleItemsPerPage(12)
+        carousel.resizeType = .visibleItemsPerPage(10)
         carousel.defaultSelectedIndex = 1
         carousel.delegate = self
         carousel.scrollType = .default
+    }
+    
+    private func gradientCarousel() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = carousel.bounds
+        let color1 = #colorLiteral(red: 0.5725490196, green: 0.7333333333, blue: 0.8823529412, alpha: 1)
+        let color2 = #colorLiteral(red: 1, green: 0.5333333333, blue: 0, alpha: 1)
+        let color3 = #colorLiteral(red: 1, green: 0.8431372549, blue: 0.6823529412, alpha: 1)
+        let color4 = #colorLiteral(red: 0.4274509804, green: 0.8039215686, blue: 1, alpha: 1)
+        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.locations = [0, 0.25, 0.75, 1]
+        carousel.layer.addSublayer(gradientLayer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        gradientView()
     }
     
     private func setupUI() {
@@ -44,6 +63,20 @@ class ExploreViewController: AuthViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(ExploreViewController.showMapView))
         mapViewImage.addGestureRecognizer(tap)
         mapViewImage.isUserInteractionEnabled = true
+    }
+    
+    private func gradientView() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        let color1 = #colorLiteral(red: 0.5725490196, green: 0.7333333333, blue: 0.8823529412, alpha: 1)
+        let color2 = #colorLiteral(red: 1, green: 0.5333333333, blue: 0, alpha: 1)
+        let color3 = #colorLiteral(red: 1, green: 0.8431372549, blue: 0.6823529412, alpha: 1)
+        let color4 = #colorLiteral(red: 0.4274509804, green: 0.8039215686, blue: 1, alpha: 1)
+        gradientLayer.colors = [color1, color2, color3, color4]
+        //        gradientLayer.locations = [0, 0.25, 0.75, 1]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+        self.view.layer.addSublayer(gradientLayer)
     }
     
     @objc private func showMapView() {
@@ -60,14 +93,25 @@ class ExploreViewController: AuthViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func labelForString(_ string: String) -> UILabel {
+    func labelForString(text string: String,index index: Int) -> UIView {
+        print(index)
+        let viewContainer = GradientView()
         let text = UILabel()
         text.text = string
         text.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        text.baselineAdjustment = .alignCenters
         text.textAlignment = .center
         text.font = .systemFont(ofSize: 14.0)
+        text.sizeToFit()
         text.numberOfLines = 0
-        return text
+        viewContainer.startColor = colors[index]
+        viewContainer.endColor = colors[index+1]
+        viewContainer.shadowColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        viewContainer.sizeToFit()
+        viewContainer.endPointX = 1
+        viewContainer.shadowBlur = 0
+        viewContainer.addSubview(text)
+        return viewContainer
     }
     
     @IBAction func unwindToGridView(sender: UIStoryboardSegue) {
@@ -89,34 +133,34 @@ class ExploreViewController: AuthViewController {
 
 extension ExploreViewController: SwiftCarouselDelegate {
     func didSelectItem(item: UIView, index: Int, tapped: Bool) -> UIView? {
-        if let animal = item as? UILabel {
-            animal.textColor = UIColor.red
-            selectedItemLabel.text = "Show photos in \(animal.text!)"
-            return animal
-        }
+//        if let animal = item as? UILabel {
+//            animal.textColor = UIColor.red
+////            selectedItemLabel.text = "Show photos in \(animal.text!)"
+//            return animal
+//        }
         
         return item
     }
     
     func didDeselectItem(item: UIView, index: Int) -> UIView? {
-        if let animal = item as? UILabel {
-            animal.textColor = .black
-            
-            return animal
-        }
+//        if let animal = item as? UILabel {
+//            animal.textColor = .black
+//
+//            return animal
+//        }
         
         return item
     }
     
     func didScroll(toOffset offset: CGPoint) {
-        selectedItemLabel.text = "Spinning up!"
+//        selectedItemLabel.text = "Spinning up!"
     }
     
     func willBeginDragging(withOffset offset: CGPoint) {
-        selectedItemLabel.text = "So you're gonna drag me now?"
+//        selectedItemLabel.text = "So you're gonna drag me now?"
     }
     
     func didEndDragging(withOffset offset: CGPoint) {
-        selectedItemLabel.text = "Oh, here we go!"
+//        selectedItemLabel.text = "Oh, here we go!"
     }
 }

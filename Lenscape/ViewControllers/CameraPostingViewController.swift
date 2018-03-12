c16db8d7021f50e858ce2389804e901b58a09bd1
Taweerat Chaiman <taweesoft@gmail.com>
2018-03-12 13:56:38 +0700

    open camera, add permission in info.plist

---
+++ Lenscape/ViewControllers/CameraPostingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CameraPostingViewController.swift
+//  Lenscape
+//
+//  Created by TAWEERAT CHAIMAN on 12/3/2561 BE.
+//  Copyright © 2561 Lenscape. All rights reserved.
+//
+
+import UIKit
+
+class CameraPostingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        openCamera()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Functions
+    private func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }else {
+            print("No camera source type")
+        }
+    }
+
+}

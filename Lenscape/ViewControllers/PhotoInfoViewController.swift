8cf6a876605e3316f270e5c97cc766ae049b955e
Taweerat Chaiman <taweesoft@gmail.com>
2018-03-21 18:03:41 +0700

    add pan gesture at informationCard

--- Lenscape/ViewControllers/PhotoInfoViewController.swift
+++ Lenscape/ViewControllers/PhotoInfoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Hero
 
 class PhotoInfoViewController: UIViewController, HeroViewControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     var image: Image?
     var uiImage: UIImage?
@@ -20,11 +20,11 @@ class PhotoInfoViewController: UIViewController, HeroViewControllerDelegate {
         super.viewDidLoad()
         self.imageView.hero.id = self.image?.thumbnailLink!
         
-//        https://github.com/lkzhao/Hero/issues/187
-        self.informationWrapper.hero.modifiers = [.duration(0.4), .translate(y: informationWrapper.bounds.height), .beginWith([.zPosition(10)]), .useGlobalCoordinateSpace]
-        
+        //        https://github.com/lkzhao/Hero/issues/187
+        self.informationWrapper.hero.modifiers = [.duration(0.4), .translate(y: informationWrapper.bounds.height*2), .beginWith([.zPosition(10)]), .useGlobalCoordinateSpace]
         self.setupUI()
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,13 +35,37 @@ class PhotoInfoViewController: UIViewController, HeroViewControllerDelegate {
         imageView.kf.setImage(with: url, placeholder: uiImage)
     }
     
-    @IBAction func dismiss(_ sender: UIPanGestureRecognizer) {
+    private func dismissView() {
+        Hero.shared.defaultAnimation = .fade
+        hero.dismissViewController()
+    }
+    
+    @IBAction func dismissBySwipe(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: nil)
+        let progress = translation.y / 2 / view.bounds.height
+        let isSwipeTopToBottom = sender.velocity(in: nil).y > 0
+        
+        if !isSwipeTopToBottom {
+            return
+        }
+        
         switch sender.state {
         case .began:
-            Hero.shared.defaultAnimation = .fade
-            hero.dismissViewController()
+            dismissView()
+        case .changed:
+            Hero.shared.update(progress)
+            let currentPos = CGPoint(x: informationWrapper.center.x, y: translation.y + informationWrapper.center.y)
+            Hero.shared.apply(modifiers: [.position(currentPos)], to: informationWrapper)
         default:
-            Hero.shared.finish()
+            if progress + sender.velocity(in: nil).y / view.bounds.height > 0.2 {
+                Hero.shared.finish()
+            }else {
+                Hero.shared.cancel()
+            }
         }
+    }
+    
+    @IBAction func dismissByTap(_ sender: UITapGestureRecognizer) {
+        dismissView()
     }
 }

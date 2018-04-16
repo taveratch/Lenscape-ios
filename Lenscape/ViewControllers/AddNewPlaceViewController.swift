1647efa4352665bf31c7ca0d75d03d3676d73ea2
Taweerat Chaiman <taweesoft@gmail.com>
2018-04-16 18:46:09 +0700

    add placename textfield

--- Lenscape/ViewControllers/AddNewPlaceViewController.swift
+++ Lenscape/ViewControllers/AddNewPlaceViewController.swift
@@ -10,41 +10,29 @@ import UIKit
 
 class AddNewPlaceViewController: UIViewController {
 
+    @IBOutlet weak var placeNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-       UIApplication.shared.statusBarStyle = .lightContent
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Automatically show keyboard
+        placeNameTextField.becomeFirstResponder()
     }
     
     @IBAction func back(_ sender: UIBarButtonItem) {
+        // Hide keyboard
+        placeNameTextField.resignFirstResponder()
+        
         dismiss(animated: true)
     }
-    
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension AddNewPlaceViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
     }
-    */
-
 }

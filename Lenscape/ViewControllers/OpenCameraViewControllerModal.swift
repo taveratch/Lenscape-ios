57dbd0b7f3acfbc562056dd837b330641fa5bb31
Thongrapee <kulaspa@gmail.com>
2018-04-16 17:43:34 +0700

    Camera Overhaul

--- Lenscape/ViewControllers/OpenCameraViewControllerModal.swift
+++ Lenscape/ViewControllers/OpenCameraViewControllerModal.swift
@@ -5,87 +5,155 @@
 //  Created by TAWEERAT CHAIMAN on 17/3/2561 BE.
 //  Copyright © 2561 Lenscape. All rights reserved.
 //
-//https://github.com/codepath/ios_guides/wiki/Creating-a-Custom-Camera-View
 
 import UIKit
 import AVFoundation
 
-class OpenCameraViewControllerModal: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class OpenCameraViewControllerModal: UIViewController {
 
     // MARK: - UI Components
-    @IBOutlet weak var cameraRollButton: UIView!
-    @IBOutlet weak var dismissButton: UIView!
-    @IBOutlet weak var cameraButton: UIView!
-    @IBOutlet weak var previewView: UIView!
+    @IBOutlet private weak var cameraRollButton: UIView!
+    @IBOutlet private weak var dismissButton: UIView!
+    @IBOutlet private weak var cameraButton: UIView!
+    @IBOutlet private weak var previewView: UIView!
     
     // MARK: - Attributes
-    var session: AVCaptureSession?
-    var stillImageOutput: AVCaptureStillImageOutput?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    private var imagePickerController = UIImagePickerController()
-    var isPortrait = true
-    var uiImageOrientation: UIImageOrientation? = UIImageOrientation.right
-    var lastOrientation = UIDeviceOrientation.portrait
+    private var sessionQueue = DispatchQueue(label: "session queue")
+    private var session: AVCaptureSession!
+    private var imageOutput: AVCapturePhotoOutput!
+    private var device: AVCaptureDevice!
+    private var videoPreviewLayer: AVCaptureVideoPreviewLayer!
     
+    private var imagePickerController = UIImagePickerController()
+    private var lastOrientation = UIDeviceOrientation.portrait
+    
+    // MARK: - Computed Properties
+    
+    private var photoSettings: AVCapturePhotoSettings {
+        let settings = AVCapturePhotoSettings()
+        settings.flashMode = .auto
+        settings.isAutoStillImageStabilizationEnabled = true
+        settings.isHighResolutionPhotoEnabled = true
+        return settings
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
     // MARK: - ViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePickerController.delegate = self
-        addShotButtonGesture()
-        addDismissButtonGesture()
-        addOpenCameraRollGesture()
-        initCamera()
-        videoPreviewLayer!.frame = view.layer.bounds
+        addGesture(for: cameraButton, with: #selector(snapPhoto))
+        addGesture(for: cameraRollButton, with: #selector(openCameraRoll))
+        addGesture(for: dismissButton, with: #selector(close))
+        sessionQueue.async { self.initCamera() }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // Subscribe Device Orientation event
-        NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
-        
-        // When user press back from Preview Controller, camera should be shown in the same orientation as it disappeared.
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(rotated),
+            name: .UIDeviceOrientationDidChange,
+            object: nil
+        )
+        sessionQueue.async {
+            do {
+                try self.device.lockForConfiguration()
+                self.device.focusMode = .continuousAutoFocus
+                self.device.unlockForConfiguration()
+            } catch { print(error) }
+            self.session.startRunning()
+        }
         setCurrentOrientation()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Unsubscribing device orientation event.
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+        sessionQueue.async { self.session.stopRunning() }
+        NotificationCenter.default.removeObserver(self, name: .UIDeviceOrientationDidChange, object: nil)
     }
-    
+
     /*
      Set current orientation to the same as it disappeared
     */
-    func setCurrentOrientation() {
+    private func setCurrentOrientation() {
         UIDevice.current.setValue(Int(lastOrientation.rawValue), forKey: "orientation")
     }
     
-    /*
-     Rotate camera roll icon and set which orientation that image has to be rotated.
-     https://stackoverflow.com/a/40263064/6344975
-     https://www.pinterest.de/pin/223983781440138942/?autologin=true
-    */
-    @objc func rotated() {
-        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft {
-            uiImageOrientation = nil
-            isPortrait = false
-            rotateCameraRollImageView(angle: Double.pi / 2)
-        }else if UIDevice.current.orientation == UIDeviceOrientation.portrait {
-            uiImageOrientation = UIImageOrientation.right
-            isPortrait = true
-            rotateCameraRollImageView(angle: 2 * Double.pi)
-        }else if UIDevice.current.orientation == UIDeviceOrientation.landscapeRight {
-            uiImageOrientation = UIImageOrientation.down
-            isPortrait = false
-            rotateCameraRollImageView(angle: 3 * Double.pi / 2)
-        }else {
-            return
+    // MARK: - UI Initialization
+    
+    private func initCamera() {
+        session = AVCaptureSession()
+        session.sessionPreset = .photo
+        imageOutput = AVCapturePhotoOutput()
+        imageOutput.isHighResolutionCaptureEnabled = true
+        device = AVCaptureDevice.default(for: .video)
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            session.beginConfiguration()
+            if session.canAddInput(input) {
+                session.addInput(input)
+                if session.canAddOutput(imageOutput) {
+                    session.addOutput(imageOutput)
+                    DispatchQueue.main.async {
+                        self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                        self.videoPreviewLayer.videoGravity = .resizeAspectFill
+                        self.videoPreviewLayer.connection?.videoOrientation = .portrait
+                        self.videoPreviewLayer.frame = self.view.layer.bounds
+                        self.previewView.layer.addSublayer(self.videoPreviewLayer)
+                    }
+                }
+            }
+            session.commitConfiguration()
         }
-        lastOrientation = UIDevice.current.orientation
+        catch {
+            print(error)
+        }
+        
     }
     
-    // MARK: - ImagePickerController functions
+    // MARK: - Camera Touch Focus
+    @IBAction private func focusAndExposeTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        let devicePoint = videoPreviewLayer.captureDevicePointConverted(
+            fromLayerPoint: gestureRecognizer.location(in: gestureRecognizer.view)
+        )
+        focus(with: .autoFocus, exposureMode: .autoExpose, at: devicePoint)
+    }
+    
+    // MARK: - Unwind
+    @IBAction func unwindToCameraAndDismiss(sender: UIStoryboardSegue) {
+        dismiss(animated: false)
+    }
+    
+    // MARK: - Objc functions (for action selector)
+    @objc
+    private func snapPhoto() {
+        imageOutput.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    @objc
+    private func openCameraRoll() {
+        present(imagePickerController, animated: true)
+    }
+    
+    @objc
+    private func close() {
+        dismiss(animated: true)
+    }
+    
+    @objc
+    private func rotated() {
+        rotateCameraRollImageView()
+        lastOrientation = UIDevice.current.orientation
+    }
+
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension OpenCameraViewControllerModal: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
@@ -93,100 +161,55 @@ class OpenCameraViewControllerModal: UIViewController, UIImagePickerControllerDe
         dismiss(animated: true)
         showPhotoPreviewController(image: selectedImage)
     }
+}
+
+// MARK: - AVCapturePhotoCaptureDelegate
+extension OpenCameraViewControllerModal: AVCapturePhotoCaptureDelegate {
     
-    // MARK: - UI Initialization
-    
-    private func initCamera() {
-        session = AVCaptureSession()
-        session!.sessionPreset = AVCaptureSession.Preset.photo
-        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-        var error: NSError?
-        var input: AVCaptureDeviceInput!
-        do {
-            input = try AVCaptureDeviceInput(device: backCamera!)
-        } catch let error1 as NSError {
-            error = error1
-            input = nil
-            print(error!.localizedDescription)
+    func photoOutput(_ output: AVCapturePhotoOutput,
+                     didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            print(error?.localizedDescription ?? "")
+            return
         }
-        if error == nil && session!.canAddInput(input) {
-            session!.addInput(input)
-            stillImageOutput = AVCaptureStillImageOutput()
-            stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
-            if session!.canAddOutput(stillImageOutput!) {
-                session!.addOutput(stillImageOutput!)
-                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session!)
-                videoPreviewLayer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
-                videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-                previewView.layer.addSublayer(videoPreviewLayer!)
-                session!.startRunning()
-            }
-            // ...
-            // The remainder of the session setup will go here...
-        }
+        let image = processImage(data: photo.fileDataRepresentation())
+        showPhotoPreviewController(image: image)
     }
     
-    private func rotateCameraRollImageView(angle: Double) {
-        UIView.animate(withDuration: 0.5, animations: {
-            self.cameraRollButton.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
-        })
-    }
-    
-    // Hide status bar
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    // MARK: - Gestures
-    
-    private func addShotButtonGesture() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(snapPhoto))
-        cameraButton.addGestureRecognizer(tap)
-        cameraButton.isUserInteractionEnabled = true
-    }
-    
-    
-    private func addDismissButtonGesture() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(close))
-        dismissButton.addGestureRecognizer(tap)
-        dismissButton.isUserInteractionEnabled = true
-    }
-    
-    private func addOpenCameraRollGesture() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(openCameraRoll))
-        cameraRollButton.addGestureRecognizer(tap)
-        cameraRollButton.isUserInteractionEnabled = true
-    }
-    
-    @objc private func openCameraRoll() {
-        present(imagePickerController, animated: true, completion: nil)
-    }
-    
-    @objc private func snapPhoto() {
-        
+    func photoOutput(_ output: AVCapturePhotoOutput,
+                     willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
         previewView.alpha = 0
-        UIView.animate(withDuration: 0.3, animations: {
+        UIView.animate(withDuration: 0.3) {
             self.previewView.alpha = 1
-        })
-        
-        if let videoConnection = stillImageOutput!.connection(with: AVMediaType.video) {
-            stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (sampleBuffer, error) -> Void in
-                if sampleBuffer != nil {
-                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer!)
-                    let dataProvider = CGDataProvider(data: imageData! as CFData )
-                    let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
-                    let image = (self.uiImageOrientation != nil) ? UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: self.uiImageOrientation!) : UIImage(cgImage: cgImageRef!)
-                    self.showPhotoPreviewController(image: image)
-                }
-            })
         }
     }
+
+}
+
+// MARK: - Utilities Functions
+extension OpenCameraViewControllerModal {
     
-    @objc private func close() {
-        dismiss(animated: true, completion: nil)
+    private func processImage(data: Data?) -> UIImage {
+        let dataProvider = CGDataProvider(data: data! as CFData)
+        let cgImageRef = CGImage(
+            jpegDataProviderSource: dataProvider!,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+        )
+        
+        var imageOrientation = UIImageOrientation.right
+        switch UIDevice.current.orientation {
+        case .landscapeLeft:
+            imageOrientation = .up
+        case .landscapeRight:
+            imageOrientation = .down
+        default:
+            break
+        }
+        
+        return UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: imageOrientation)
     }
-    
-    // MARK: - Misc
     
     private func showPhotoPreviewController(image: UIImage?) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -197,10 +220,62 @@ class OpenCameraViewControllerModal: UIViewController, UIImagePickerControllerDe
         vc.hero.modalAnimationType = .fade
         present(vc, animated: true)
     }
-
-    // MARK: - Unwind
-    @IBAction func unwindToCameraAndDisiss(sender: UIStoryboardSegue) {
-        dismiss(animated: false)
+    
+    private func rotateCameraRollImageView() {
+        var angle: Double
+        switch UIDevice.current.orientation {
+        case .landscapeLeft:
+            angle = Double.pi / 2
+        case .landscapeRight:
+            angle = 3/2 * Double.pi
+        default:
+            angle = 2 * Double.pi
+        }
+        UIView.animate(withDuration: 0.4, animations: {
+            self.cameraRollButton.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        })
     }
+    
+    private func addGesture(for view: UIView, with action: Selector?) {
+        let tap = UITapGestureRecognizer(target: self, action: action)
+        view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
+    }
+    
+    private func focus(with focusMode: AVCaptureDevice.FocusMode,
+                       exposureMode: AVCaptureDevice.ExposureMode, at devicePoint: CGPoint) {
+        sessionQueue.async {
+            do {
+                try self.device.lockForConfiguration()
 
+                if self.device.isFocusPointOfInterestSupported && self.device.isFocusModeSupported(focusMode) {
+                    self.device.focusPointOfInterest = devicePoint
+                    self.device.focusMode = focusMode
+                }
+                
+                if self.device.isExposurePointOfInterestSupported && self.device.isExposureModeSupported(exposureMode) {
+                    self.device.exposurePointOfInterest = devicePoint
+                    self.device.exposureMode = exposureMode
+                }
+                
+                self.device.isSubjectAreaChangeMonitoringEnabled = true
+                self.device.unlockForConfiguration()
+            } catch {
+                print("Could not lock device for configuration: \(error)")
+            }
+        }
+    }
+    
 }
+
+// MARK: - References
+/*
+Migrate from AVCaptureStillImageOutput -> AVCapturePhotoOutput
+ https://stackoverflow.com/questions/37869963/how-to-use-avcapturephotooutput?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+Retain current device orientation on view change
+ https://stackoverflow.com/a/40263064/6344975
+ https://www.pinterest.de/pin/223983781440138942/?autologin=true
+Create custom camera view
+ https://github.com/codepath/ios_guides/wiki/Creating-a-Custom-Camera-View
+*/
+

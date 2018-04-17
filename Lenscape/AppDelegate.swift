749cf22da61002fbfe6c55d59219397daa7f9c84
Thongrapee Panyapatiphan <kulaspa@gmail.com>
2018-04-18 00:15:06 +0700

    Complete notification setup

--- Lenscape/AppDelegate.swift
+++ Lenscape/AppDelegate.swift
@@ -29,18 +29,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Firebase
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
         
-        let changeViewController = { (identifier: String) -> Void in
+        let changeViewController = { (identifier: Identifier) -> Void in
             let storyBoard = UIStoryboard.init(name: "Main", bundle: nil)
-            let navigationController: UINavigationController? = (self.window?.rootViewController as? UINavigationController)
-            let vc = storyBoard.instantiateViewController(withIdentifier: identifier)
+            let navigationController = (self.window?.rootViewController as? UINavigationController)
+            let vc = storyBoard.instantiateViewController(withIdentifier: identifier.rawValue)
             navigationController?.pushViewController(vc, animated: false)
         }
         if UserController.getCurrentUser() != nil {
-            changeViewController(Identifier.MainTabBarController.rawValue)
+            changeViewController(.MainTabBarController)
         }
-        UserController.isLoggedIn().catch{ error in
-            changeViewController(Identifier.SigninViewController.rawValue)
+        UserController.isLoggedIn().catch { error in
+            changeViewController(.SigninViewController)
             os_log("User is not signed in", log: .default, type: .debug)
         }
         
@@ -113,5 +114,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+        
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
 }
 

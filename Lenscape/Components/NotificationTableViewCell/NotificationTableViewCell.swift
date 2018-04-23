fb54637b2a1f950a7bacbe98fe4f192b249e7f25
Thongrapee Panyapatiphan <kulaspa@gmail.com>
2018-04-23 13:51:42 +0700

    Change icons, and add data model

--- Lenscape/Components/NotificationTableViewCell/NotificationTableViewCell.swift
+++ Lenscape/Components/NotificationTableViewCell/NotificationTableViewCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NotificationTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var body: UILabel!
+    @IBOutlet weak var time: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
